5bf90258cbcb2e6ddc5a928e3131b62f8a2da71a
ADRIA SANCHEZ LOPEZ <45901887m@AD.FPMISLATA.COM>
2021-01-15 11:58:48 +0100

    Cosas de servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppcomponentComponent } from './appcomponent/appcomponent.component';
 import { CardTrabajadorComponent } from './card-trabajador/card-trabajador.component';
+import { ServicioTrabajadorService } from "./servicios/servicio-trabajador.service";
+import { TrabajadoresComponent } from './trabajadores/trabajadores.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppcomponentComponent,
-    CardTrabajadorComponent
+    CardTrabajadorComponent,
+    TrabajadoresComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ServicioTrabajadorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
